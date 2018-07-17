fba397c7ffdef37dc1e5a5b5806ae15c833a1608
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-17 13:32:03 +0200

    add calculateTotalPoints

---
+++ calculateTotalPoints.js
@@ -0,0 +1,13 @@
+const calculateDistancePoints = require('./calculateDistancePoints');
+const calculateStylePoints = require('./calculateStylePoints');
+
+const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, windFactor, gateFactor) => {
+  const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
+  const stylePoints = calculateStylePoints(styleNotes);
+
+  const totalPoints = distancePoints + stylePoints + windFactor + gateFactor;
+
+  return totalPoints.toFixed(1);
+}
+
+module.exports = calculateTotalPoints;
